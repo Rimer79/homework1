745f8e64f83b6df628388856423e49131e0a0fa0
Andrey Lazarchik <anlaz@tut.by>
2017-04-27 19:19:05 +0000

    исправилусловие сравнения

--- polindrom.js
+++ polindrom.js
@@ -1,27 +1,32 @@
-// JavaScript File
 
-// надо проверить фразу на полиндром
+
 var fraza = prompt("напишите любую фразу , \n мы проверим ее на полиндром.");
-   // приводим к нижнему регистру
-    fraza = fraza.toLowerCase();
-  //  alert(fraza);
-    fraza = fraza.split('');
+// приводим к нижнему регистру
+fraza = fraza.toLowerCase();
+//  alert(fraza);
+fraza = fraza.split('');
 //alert(fraza);
-                for (var i=0 ; i<fraza.length ; i++) {
-                    if ( fraza[i]===' '){
-                    fraza.splice(i,1);
-                    }
-                }
+for (var i=0 ; i<fraza.length ; i++) {
+    if ( fraza[i]===' '){
+        fraza.splice(i,1);
+    }
+}
 alert ('фраза бeз пробелов' +fraza);
-alert (fraza.reverse());
-var polindrom = fraza.reverse();
-   function  sravnenie( fraza,polindrom) {
-       for (var i=0 ; i<fraza.length ; i++){
-           if (fraza[i]=== polindrom[i]){
-               alert ('фраза  полиндром');
 
-           }
-           else { alert('фраза не полиндром'); break ;}
-       }
-   }
-sravnenie(fraza, polindrom);
+
+function  sravnenie( fraza) {
+    var palindrom = true;
+    var bukaff = fraza.length ;
+    for (var i = 0 ; i<bukaff/2 ; i++){
+        if (fraza[i]!== fraza[bukaff-1-i]){
+            palindrom = false;
+            break ;
+        }
+    }
+    return palindrom ;
+}
+ var result = sravnenie(fraza);
+if (result) {
+    alert('фраза  полиндром');
+} else { alert ('Фраза не  полиндром');
+}
