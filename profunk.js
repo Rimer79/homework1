38db7ba8b1d4443e30ca6ce40c5d709575be83ab
Andrey Lazarchik <anlaz@tut.by>
2017-04-28 06:44:54 +0000

    исправил во втором

--- profunk.js
+++ profunk.js
@@ -1,15 +1,6 @@
 // JavaScript File
 
-function myFun(...args) {
-    var posl = arguments.length-1,
-    fan = arguments[posl],
-    result = [];
-    for (var i=0; i<posl ;i++){
-        result[i]= fan (args[i]);
 
-    }
-    return result;
-}
 var finish ;
 
 finihs = myFun('abc','ert','rty', 'bar' ,function(str){
